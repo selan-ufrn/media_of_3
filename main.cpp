db6f307a5abaa4a707449cdacf7e16ee2a2e0e2d
Selan R dos Santos <selan.santos@ufrn.br>
2023-03-09 10:24:02 -0300

    iniciei funcao to_string()

--- main.cpp
+++ main.cpp
@@ -1,10 +1,24 @@
-#include <iostream>
+#include <iostream> // acessar o cout cin
+#include <string> // acessar o strin
+#include <sstream> // acessar o ostrinstream
+
+int func(int x, int y){
+
+}
+
+std::string to_string(int V[], std::size_t len){
+  std::ostringstream oss;
+
+
+}
 
 int main (int argc, char *argv[])
 {
   int V[]{5, 0, 0, 3, 0, 0, 8};
   std::size_t len{7};
 
-  
+  // Imprimir o vetor na saida padrao.
+  std::cout << ">>> Vetor original: " << to_string(V,len) << '\n';
+
   return 0;
 }
